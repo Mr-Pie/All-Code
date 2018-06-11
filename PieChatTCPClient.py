fba55b20afd0df0f9f713697cb201fc9dd738ebe
Pie <nathankoutoulas@gmail.com>
2018-06-11 16:36:37 +1000

    -added sound for incoming message -added exception if socket cant connect, will continuously try to reconnect

--- PieChatTCPClient.py
+++ PieChatTCPClient.py
@@ -5,37 +5,48 @@ import tkinter
 from tkinter import *
 from time import sleep
 import threading
+import winsound
+import sys
 
 TCP_IP = '192.168.0.88'
 #TCP_IP = '127.0.0.1'
 TCP_PORT = 29200
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 8192
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
 
-print('Initialising')
+#print('Initialising')
 
 top = tkinter.Tk()
 Text = tkinter.Text(top, height = 10, width = 40)
 Text.grid(column = 1, row = 1)
 TextInput = tkinter.Entry(top, bd =1, width = 35)
 TextInput.grid(column = 1, row = 2)
-               
-# Code to add widgets will go here...
+
+def TCPConnect():
+    #print("trying to connect")
+    try:
+        s.connect((TCP_IP, TCP_PORT))
+    except socket.error:
+        #print("Cant open socket :(")
+        TCPConnect()
+
+def MessageFb():
+    winsound.PlaySound('C:/Windows/Media/chimes.wav', winsound.SND_ASYNC)
 
 def Listener():
     global s
     data = s.recv(BUFFER_SIZE)
     Text.insert(END,data)
     Text.see("end")
-    print(data)
-    print('repeating thread')
+    #print(data)
+    #print('repeating thread')
+    MessageFb()
     Listener()
     
 def MagicButton():
     if len(TextInput.get()) != 0:   
         #Text.insert(INSERT, "Hi Tomo\n")
-        print('Sending message')
+        #print('Sending message')
         LineFeed = '\n'
         MESSSAGE = bytes(TextInput.get()+LineFeed, 'utf-8')
         #MESSSAGE = b'HELLO\n'
@@ -46,10 +57,12 @@ def MagicButton():
         TextInput.delete(0,END)
 
 def PressedEnter(junk):
-    print('Enter Pressed')
+    #print('Enter Pressed')
     MagicButton()
 
+TCPConnect()
 MagicButton()
+
 
 TCPListen = threading.Thread(target = Listener)
 TCPListen.start()
